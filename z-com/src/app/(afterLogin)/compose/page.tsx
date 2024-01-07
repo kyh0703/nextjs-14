2f851700bb5451f3a79bbe6d5bde69682b7b7f0c
kyh0703 <kyh0703@nate.com>
2024-01-07 23:06:58 +0900

    Feat: Left Right section

--- z-com/src/app/(afterLogin)/compose/page.tsx
+++ z-com/src/app/(afterLogin)/compose/page.tsx
@@ -1,3 +1,3 @@
 export default function Page() {
-  return <div>message</div>;
+  return <div>compose</div>;
 }
--- z-com/src/app/(beforeLogin)/login/page.tsx
+++ z-com/src/app/(beforeLogin)/login/page.tsx
@@ -1,5 +1,20 @@
-import { redirect } from 'next/navigation';
+'use client';
+
+import Main from '@/app/(beforeLogin)/_component/Main';
+import { useRouter } from 'next/navigation';
 
 export default function Login() {
-  redirect('/i/flow/login');
+  const router = useRouter();
+  router.replace('/i/flow/login');
+  return Main;
 }
+
+// modal창으로 돌아갈때 redirect는 서버 컴포넌트 용이기에 배경을 넘길 수 없어 router로 대체
+
+// router.push
+// localhost:3001 -> localhost:3001/login -> localhost:3001/i/flow/login
+// 직전 페이지로 돌아갈 수 있음
+
+// router.replace
+// localhost:3001 -> localhost:3001/login -> localhost:3001/i/flow/login
+// 직전 페이지 다음으로 돌아갈 수 있음
